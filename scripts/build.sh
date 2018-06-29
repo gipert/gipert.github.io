edf788f050d75a959d6562652eac2ed52a4bd619
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2018-06-29 18:37:15 +0200

    removed linux icon, updated scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-cd $(dirname $0)/..
-set -x
-
-NODE_ENV="production" node_modules/.bin/webpack --progress --hide-modules
+node_modules/.bin/webpack --progress --hide-modules
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-cd $(dirname $0)/..
-set -x
-
-scripts/build.sh
-
 rsync -av -e "ssh -A pertoldi@gate.pd.infn.it ssh" dist/ pertoldi@lx.pd.infn.it:/userswww/pertoldi/
